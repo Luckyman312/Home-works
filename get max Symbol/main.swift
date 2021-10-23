c6177addb28f5bcb1eb5f3fed278b6868c18dc26
Luckyman312 <shbahtiyar@gmail.com>
2021-10-23 21:15:47 +0600

    Initial Commit

---
+++ get max Symbol/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  get max Symbol
+//
+//  Created by Baktyiar Shamyrbaev on 17/10/21.
+//
+
+import Foundation
+
+func getMaxNumber(number:[Int]) -> Int {
+    var maxNmber = number.max();
+    
+    print("Max number is - \(maxNmber!)")
+    return maxNmber!
+}
+
+getMaxNumber(number: [1,3,4,5,7,10])
